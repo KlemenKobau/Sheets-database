9bff872f1fe0cbdb609be1aac840bb3279c8bf1f
Klemen Kobau <klemen.kobau@gmail.com>
2024-02-06 20:13:11 +0100

    feat: work on macros

--- sheets_database_derive/src/lib.rs
+++ sheets_database_derive/src/lib.rs
@@ -1,6 +1,16 @@
+use std::ops::Deref;
+
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput};
+use proc_macro2::Ident;
+use quote::{quote, ToTokens};
+use syn::{
+    parse_macro_input, Data, DataStruct, DeriveInput, Field, FieldsNamed, PathSegment, Type,
+};
+
+struct Property<'a> {
+    name: &'a Ident,
+    type_name: &'a Ident,
+}
 
 #[proc_macro_derive(Entity)]
 pub fn derive_entity(input: TokenStream) -> TokenStream {
@@ -8,23 +18,62 @@ pub fn derive_entity(input: TokenStream) -> TokenStream {
 
     dbg!("{:?}", &input);
 
+    let ident = &input.ident;
     let data = &input.data;
 
     if let Data::Struct(data_struct) = data {
-        todo!();
     } else {
-        quote! {
+        return quote! {
             compile_error!("Currently only works for structs");
         }
-        .into()
+        .into();
     }
+
+    quote! {
+        impl Entity for #ident {
+            fn save(&self) {
+
+            }
+
+            fn query(match_function: dyn Fn(&Self) -> bool) -> Vec<Self>
+            where
+                Self: std::marker::Sized {
+                Vec::new()
+            }
+        }
+    }
+    .into()
 }
 
 fn implement_for_data(data_str: &DataStruct) {
     let fields = &data_str.fields;
 
     match fields {
-        syn::Fields::Named(_) => todo!(),
+        syn::Fields::Named(named_fields) => {
+            let props = parse_named_fields(named_fields);
+        }
         syn::Fields::Unnamed(_) | syn::Fields::Unit => todo!(),
     }
+}
+
+fn parse_named_fields(named_fields: &FieldsNamed) -> Vec<Property<'_>> {
+    let mut fields = Vec::new();
+
+    for Field { ident, ty, .. } in named_fields.named.iter() {
+        let field_name = ident.as_ref().unwrap();
+
+        if let Type::Path(path) = ty {
+            let path = &path.path;
+            for PathSegment { ident, .. } in path.segments.iter() {
+                let property = Property {
+                    name: field_name,
+                    type_name: ident,
+                };
+
+                fields.push(property);
+            }
+        }
+    }
+
+    fields
 }
--- test_suite/tests/test_macros.rs
+++ test_suite/tests/test_macros.rs
@@ -1,6 +1,8 @@
+use sheets_database::entity::Entity;
 use sheets_database_derive::Entity;
 
 #[derive(Entity)]
 struct ExampleString {
     test: String,
+    test2: String,
 }
